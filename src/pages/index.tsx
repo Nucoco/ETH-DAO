9328b3941a71bbcfea69159fa9aea966349c6cff
Nucoco <yuuta11511997@gmail.com>
2024-04-27 15:15:56 +0000

    メンバー一覧表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import {
   useContract,
 } from '@thirdweb-dev/react';
 import type { NextPage } from 'next';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 import styles from '../styles/Home.module.css';
 
@@ -19,9 +19,15 @@ const Home: NextPage = () => {
   const chain = useChain();
 
   // コントラクトを初期化
-  const editionDrop = useContract(
+  const erc1155 = useContract(
     '0xAf8990ABe1E6857C4DCD0cf5dF486fE276799Cc6',
     'edition-drop',
+  ).contract;
+
+  // トークンコントラクトの初期化
+  const erc20 = useContract(
+    '0xA9ED9DCF7941c499338a3d83e50133F90870AE67',
+    'token',
   ).contract;
 
   // ユーザーがメンバーシップ NFT を持っているかどうかを知るためのステートを定義
@@ -29,15 +35,62 @@ const Home: NextPage = () => {
 
   // NFT をミンティングしている間を表すステートを定義
   const [isClaiming, setIsClaiming] = useState(false);
+  // メンバーごとの保有しているトークンの数をステートとして宣言
+  const [memberTokenAmounts, setMemberTokenAmounts] = useState<any>([]);
 
-  useEffect(() => {
-        if (!address) {
+  // DAO メンバーのアドレスをステートで宣言
+  const [memberAddresses, setMemberAddresses] = useState<string[] | undefined>([]);
+
+  // アドレスの長さを省略してくれる便利な関数
+  const shortenAddress = (str: string) => {
+    return str.substring(0, 6) + '...' + str.substring(str.length - 4);
+  };
+
+  // メンバーシップを保持しているメンバーの全アドレスを取得します
+  const getAllAddresses = () => {
+    if (!hasClaimedNFT) {
       return;
     }
 
-    const checkBalance = async () => {
+    // 先ほどエアドロップしたユーザーがここで取得できます（発行された tokenID 0 のメンバーシップ NFT）
+    (async () => {
       try {
-        const balance = await editionDrop!.balanceOf(address, basicMemberTokenId);
+        const memberAddresses = await erc1155?.history.getAllClaimerAddresses(
+          0
+        );
+        setMemberAddresses(memberAddresses);
+        console.log('🚀 Members addresses', memberAddresses);
+      } catch (error) {
+        console.error('failed to get member list', error);
+      }
+    })();
+  }
+
+  // 各メンバーが保持するトークンの数を取得します
+  const getAllBalances = () => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+
+    (async () => {
+      try {
+        const amounts = await erc20?.history.getAllHolderBalances();
+        setMemberTokenAmounts(amounts);
+        console.log('👜 Amounts', amounts);
+      } catch (error) {
+        console.error('failed to get member balances', error);
+      }
+    })();
+  }
+
+  const checkIsMember = () => {
+    if (!address) {
+      return;
+    }
+
+    (async () => {
+      try {
+        const balance = await erc1155!.balanceOf(address, basicMemberTokenId);
         if (balance.gt(0)) {
           setHasClaimedNFT(true);
           console.log('🌟 this user has a membership NFT!');
@@ -49,15 +102,35 @@ const Home: NextPage = () => {
         setHasClaimedNFT(false);
         console.error('Failed to get balance', error);
       }
-    };
-    checkBalance();
-  }, [address, editionDrop]);
+    })();
+  }
+
+  useEffect(getAllAddresses, [hasClaimedNFT, erc1155?.history]);
+  useEffect(getAllBalances, [hasClaimedNFT, erc20?.history]);
+  useEffect(checkIsMember, [address, erc1155]);
+
+  // memberAddresses と memberTokenAmounts を 1 つの配列に結合します
+  const memberList = useMemo(() => {
+    return memberAddresses?.map((address) => {
+      // memberTokenAmounts 配列でアドレスが見つかっているかどうかを確認します
+      // その場合、ユーザーが持っているトークンの量を返します
+      // それ以外の場合は 0 を返します
+      const member = memberTokenAmounts?.find(
+        ({ holder }: {holder: string}) => holder === address,
+      );
+
+      return {
+        address,
+        tokenAmount: member?.balance.displayValue || '0',
+      };
+    });
+  }, [memberAddresses, memberTokenAmounts]);
 
   const mintNft = async () => {
     try {
       setIsClaiming(true);
-      await editionDrop!.claim(String(basicMemberTokenId), 1);
-      console.log(`🌊 Successfully Minted! Check it out on etherscan: https://sepolia.etherscan.io/address/${editionDrop!.getAddress()}`);
+      await erc1155!.claim(String(basicMemberTokenId), 1);
+      console.log(`🌊 Successfully Minted! Check it out on etherscan: https://sepolia.etherscan.io/address/${erc1155!.getAddress()}`);
       setHasClaimedNFT(true);
     } catch (error) {
       setHasClaimedNFT(false);
@@ -105,11 +178,34 @@ const Home: NextPage = () => {
   const forMember = () => {
     return (
       <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>🍪DAO Member Page</h1>
-        <p>Congratulations on being a member</p>
-      </main>
-    </div>
+        <main className={styles.main}>
+          <h1 className={styles.title}>🍪DAO Member Page</h1>
+          <p>Congratulations on being a member</p>
+          <div>
+          <div>
+            <h2>Member List</h2>
+            <table className="card">
+              <thead>
+                <tr>
+                  <th>Address</th>
+                  <th>Token Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                {memberList!.map((member) => {
+                  return (
+                    <tr key={member.address}>
+                      <td>{shortenAddress(member.address)}</td>
+                      <td>{member.tokenAmount}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+        </main>
+      </div>
     );
   }
 
