87f9193efb8e7ec9f2565236c5059190d9d3e42a
Nucoco <yuuta11511997@gmail.com>
2024-04-27 13:21:21 +0000

    ごっつエラー出て動かなかったのでsdkアプデして対応

--- src/scripts/module.ts
+++ src/scripts/module.ts
@@ -1,4 +1,4 @@
-export const editionDropAddress = '';
+export const editionDropAddress = '0xAf8990ABe1E6857C4DCD0cf5dF486fE276799Cc6';
 export const ERCTokenAddress = '';
 export const governanceAddress = '';
 export const ownerWalletAddress = '0x7309b27549776Bb90d7fDC1a398Cb701DE05f561';
